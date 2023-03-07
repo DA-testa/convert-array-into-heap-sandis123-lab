37a890dfec63320af128ac79a1f967fdee56eae3
221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-03-07 22:30:45 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,44 +1,22 @@
 # python3
-
-
 def build_heap(data):
-    swap count = 0
-    swap_list = []
-    size = n - 1
-    for i in range(floor(n/2), -1, -1):
-        swap_count, swap_list, data = sift_down(i, size, data, swap_count, swap_list)
-    return swap_count, swap_list
-
-
-def sift_down(i, size, data, swap_count, swap_list):
-    minIndex = i
-    l= LeftChild (i + 1)
-    if 1 <= size and data[1] < data[minIndex]:
-        minIndex = 1
-    r = RightChild(i +1)
-    if r <= size and data[r] < data[minIndex]:
-        minIndex = r
-    if i != minIndex:
-        data[i], data[minIndex] = data[minIndex], data[i]
-        swap_count += 1
-        swap_list.append((i, minIndex))
-        swap_count, swap_list, data= sift_down(minIndex, size, data, swap_count, swap_list)
-   return swap_count, swap_list, data
-
-def LeftChild(i):
-    return 2*i-1
-def RightChild(i):
-    return 2 * 1
+    swaps = []
+    
+    return swaps
 def main():
-    n = int(input(()
+    
+    n = input(input())
     data = list(map(int, input().split()))
+    
     assert len(data) == n
     
-    swap_count, swap_list = build_heap(n, data)
+    swaps = build_heap(data)
     
-    print(swap_count)
-    for swap in swap_list:
-        print(swap[0], swap[1])
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+        
+   
                   
 if __name__ == "__main__":
     main()
