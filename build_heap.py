7c985c23056529e22af5fed0f400abdfb566bded
221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-03-07 23:26:21 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,22 +1,33 @@
 # python3
-def build_heap(data):
+def sift_down(arr, i, n, swaps):
+    min_index = i
+    left_child = 2*i + 1
+    right_child = 2*i + 2
+    
+    if left_child < n and arr[left_child] < arr[min_index]:
+        min_index = left_child
+    
+    if right_child < n and arr[right_child] < arr[min_index]:
+        min_index = right_child
+    
+    if i != min_index:
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        swaps.append((i, min_index))
+        sift_down(arr, min_index, n, swaps)
+
+def build_heap(arr):
+    n = len(arr)
     swaps = []
-    
+    for i in range(n//2 - 1, -1, -1):
+        sift_down(arr, i, n, swaps)
     return swaps
-def main():
-    
-    n = input(input())
-    data = list(map(int, input().split()))
-    
-    assert len(data) == n
-    
-    swaps = build_heap(data)
-    
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-        
-   
-                  
-if __name__ == "__main__":
-    main()
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+swaps = build_heap(arr)
+
+print(len(swaps))
+for swap in swaps:
+    print(swap[0], swap[1])
+
