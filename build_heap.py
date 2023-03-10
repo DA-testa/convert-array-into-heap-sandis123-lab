acc86f3a5a07f7336f948b06831b835c49a5b904
221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-03-10 03:12:17 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,33 +1,46 @@
 # python3
-def sift_down(arr, i, n, swaps):
-    min_index = i
-    left_child = 2*i + 1
-    right_child = 2*i + 2
-    
-    if left_child < n and arr[left_child] < arr[min_index]:
-        min_index = left_child
-    
-    if right_child < n and arr[right_child] < arr[min_index]:
-        min_index = right_child
-    
-    if i != min_index:
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-        swaps.append((i, min_index))
-        sift_down(arr, min_index, n, swaps)
-
-def build_heap(arr):
-    n = len(arr)
+def heap(data):
     swaps = []
-    for i in range(n//2 - 1, -1, -1):
-        sift_down(arr, i, n, swaps)
-    return swaps
-
-n = int(input())
-arr = list(map(int, input().split()))
-
-swaps = build_heap(arr)
+    n = len(data)
+    for i in range (n // 2, -1, -1):
+        j = i
+        while True:
+            right = j * 2 + 2
+            left = j * 2 + 1
+            smallest = j 
+            if right < n and data[right] < data[smallest]:
+                smallest = right
+            if left < n and data[left] < data[smallest]:
+                smallest = left
+            if smallest != j:
+                swaps.append((j, smallest))
+                data[j], data[smallest] = data[smallest], data[j]
+                j = smallest
+            else:
+                break
+                return swaps
+def main():
+    mode = input()
+    if "I" in mode:
+        ipt= int(input())
+        data = list(map(int, input().split()))
+   elif "F" in mode:
+    filename = input()
+    if 'a' in filename:
+        return
+    with open(f"tests/{filename}") as f:
+        ipt = int(f.readline())
+        data = list(map(int, f.readline().split()))
+        assert len(data) == n
+swaps = build_heap(data)
 
 print(len(swaps))
-for swap in swaps:
-    print(swap[0], swap[1])
+for i, j in swaps:
+    print(i, j)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
 
