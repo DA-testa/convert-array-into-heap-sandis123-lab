efe43d1650a4d586b9f7b0d4f2d27d7f1a587eb1
221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-03-10 03:29:34 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,13 @@
 # python3
 def heap(data):
     swaps = []
-    ipt = len(data)
-    for i in range (n // 2, -1, -1):
+    n = len(data)
+    for i in range(n // 2, -1, -1):
         j = i
         while True:
             right = j * 2 + 2
             left = j * 2 + 1
-            smallest = j 
+            smallest = j
             if right < n and data[right] < data[smallest]:
                 smallest = right
             if left < n and data[left] < data[smallest]:
@@ -20,28 +20,25 @@ def heap(data):
                 break
     return swaps
 
+
 def main():
     mode = input()
     if "I" in mode:
-        n= int(input())
+        n = int(input())
         data = list(map(int, input().split()))
     elif "F" in mode:
         filename = input()
-         if 'a' in filename:
-             return
-    with open(f"tests/{filename}") as f:
-        n = int(f.readline())
-        data = list(map(int, f.readline().split()))
-        assert len(data) == n
-swaps = heap(data)
+        if 'a' in filename:
+            return
+        with open(f"tests/{filename}") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+            assert len(data) == n
+    swaps = heap(data)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
-print(len(swaps))
-for i, j in swaps:
-    print(i, j)
-    
-    
+
 if __name__ == "__main__":
     main()
-    
-    
-
